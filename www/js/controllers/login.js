3420c8f2c081879c1057b6211a5a2763a454c089
Pam Gaiguer <pamella.gaiguer@gmail.com>
2017-09-16 00:22:42 +1000

    adding new pages for admin

--- www/js/controllers/login.js
+++ www/js/controllers/login.js
@@ -7,8 +7,8 @@ angular.module('doismaisdois.controllers.login', [])
   $scope.vm = {
     'login':'',
     'pwd':'',
-    'isLoading':false      
-  } 
+    'isLoading':false
+  }
 
   $scope.init = function () {
     console.log("startou o login");
@@ -16,35 +16,26 @@ angular.module('doismaisdois.controllers.login', [])
     $rootScope.header_login = true;
 
     if (UserServices.currentUser.usu_id != undefined) $location.path('/admin');
+  }
 
-
-  }  
-
-  $scope.login = function(form) {      
+  $scope.login = function(form) {
     console.log('Form Valid!');
     $scope.vm.isLoading = true;
-    
+
     UserServices.deleteUserLocalStorage();
 
     UserServices.login($scope.vm.login,$scope.vm.pwd)
     .then(function(success){
-      
-      if (success == 0) {        
+
+      if (success == 0) {
         $scope.vm.isLoading = false;
         $scope.vm.ready = true;
         return;
       }
-
-            
       UserServices.saveUserLocalStorage();
-
-
       $location.path('/admin');
     });
-  
   }
-
-  
   $scope.esqueceuSenha = function(){}
 
 });
