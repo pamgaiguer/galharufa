f95b76faab71a042e9190f74459712057792b6f3
Pam Gaiguer <pamella.gaiguer@gmail.com>
2018-01-12 01:38:34 +1000

    removing one scroll casting home missing casting details

--- www/gulpfile.js
+++ www/gulpfile.js
@@ -18,7 +18,6 @@ var paths = {
         './js/libs/angular-route.min.js',
         './js/libs/materialize.min.js',
         './js/libs/ng-file-upload.min.js',
-        './bower_components/onepage-scroll/jquery.onepage-scroll.min.js',
         './js/libs/',
         './js/app.js',
         './js/services/*.js',
@@ -28,7 +27,6 @@ var paths = {
     css: [
         './bower_components/materialize/dist/css/materialize.min.css',
         './bower_components/font-awesome/font-awesome.min.css',
-        './bower_components/onepage-scroll/onepage-scroll.css',
         './styles/**/*min.css'
     ]
 };
