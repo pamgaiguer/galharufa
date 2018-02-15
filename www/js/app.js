1140592a55f924cb6c97ad9928d20430ecdafc05
AustinFelipe <austin.felipe@live.com>
2018-02-15 01:33:05 -0200

    Removing node_modules

--- www/js/app.js
+++ www/js/app.js
@@ -43,9 +43,9 @@ app.run(function($rootScope, $timeout, UserServices, $location, $route) {
 
 app.constant('Configs', {
   appToken:'',
-  // apiUrl:'http://localhost:3000/'
+  apiUrl:'http://localhost:3000/'
   //apiUrl:'http://agenciagalharufa.com.br/'
-  apiUrl:'https://agenciagalharufa.com.br/'
+  //apiUrl:'https://agenciagalharufa.com.br/'
 });
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
