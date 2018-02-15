8a055797d10b130d03c3e9016512af08e1079e29
AustinFelipe <austin.santos@moveleiros.com.br>
2018-02-15 13:19:03 -0200

    Bullshit removed

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ var app = express();
 
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  //res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  //res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
