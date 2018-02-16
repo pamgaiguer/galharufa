72916e49d197d6e24cee93e8e0afa95a0e7210d4
Pam Gaiguer <pamella.gaiguer@gmail.com>
2018-02-16 12:37:43 +1000

    fixing and adding casting-show in app.js

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  
+
   id = req.query.id;
-  req.getConnection(function(err,connection){    
+  req.getConnection(function(err,connection){
 
     if (id) {
 
@@ -27,13 +27,18 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.post('/autenticar', function(req, res, next) {  
-  
+router.post('/autenticar', function(req, res, next) {
+
   login = req.query.l;
   senha = req.query.s;
 
-  req.getConnection(function(err,connection){    
-  connection.query('SELECT * FROM usuarios where usu_login = ? and usu_senha = ?',[login, senha],function(err,result){        
+  req.getConnection(function(err,connection){
+    if (err) {
+      console.error(err)
+      throw err
+    }
+
+    connection.query('SELECT * FROM usuarios where usu_login = ? and usu_senha = ?',[login, senha],function(err,result){
         if(err) return res.status(400).json(err);
         return res.status(200).json(result);
     });
@@ -42,12 +47,12 @@ router.post('/autenticar', function(req, res, next) {
 
 
 router.post('/adicionar', function(req, res, next) {
-  
+
   nome = req.query.n;
   login = req.query.l;
   senha = req.query.s;
 
-  req.getConnection(function(err,connection){    
+  req.getConnection(function(err,connection){
   connection.query('insert into usuarios (usu_nome, usu_login, usu_senha, usu_tipo) values (?, ?, ?, ?)',[nome, login, senha, 1],function(err,result){
         if(err) return res.status(400).json(err);
         return res.status(200).json(result);
@@ -56,10 +61,10 @@ router.post('/adicionar', function(req, res, next) {
 });
 
 router.post('/excluir', function(req, res, next) {
-  
+
   id = req.query.id;
 
-  req.getConnection(function(err,connection){    
+  req.getConnection(function(err,connection){
   connection.query('delete from usuarios where usu_id = ?',[id],function(err,result){
         if(err) return res.status(400).json(err);
         return res.status(200).json(result);
--- www/js/controllers/adm-casting-show.js
+++ www/js/controllers/adm-casting-show.js
@@ -1,5 +1,19 @@
 angular.module('galharufa.controllers.adm-casting-show', [])
 
-.controller('AdminCastingShowCtrl', function($scope) {
+.controller('AdminCastingShowCtrl', function($scope, $rootScope) {
 
-})
+    $scope.vm = {
+        'isLoading':false,
+        'casting': []
+    }
+
+
+    $scope.init = function () {
+        console.log("startou o adm Casting show");
+        $rootScope.headerId = 2;
+        $rootScope.hold = false;
+        $('body').css('background-color', '#eee');
+        $rootScope.session_title= 'Galharufa Dashboard - Casting Visualizar';
+    }
+
+});
