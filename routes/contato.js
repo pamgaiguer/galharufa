bcfcc69571914e02003964aef3cfb8d3daf0a22a
Giovanni Kenji Shiroma <kenjishiromajp@gmail.com>
2019-06-25 15:22:46 +1000

    Getting const

--- routes/contato.js
+++ routes/contato.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const nodemailer = require('nodemailer')
+const nodemailer = require('nodemailer');
 
 router.post('/', function (req, res, next) {
-    const nome = req.body.nome
-    const email = req.body.email
-    const telefone = req.body.telefone
-    const mensagem = req.body.mensagem
+    const {nome, email, telefone, mensagem} = req.body;
 
     const mailOptions = {
         from: 'Galharufa Contato <contato@agenciagalharufa.com.br>',
@@ -17,7 +14,7 @@ Email: ${email}
 Telefone: ${telefone}
 Mensagem: ${mensagem}
         `
-    }
+    };
 
     const transporter = nodemailer.createTransport({
         host: 'smtp.umbler.com',
@@ -26,12 +23,12 @@ Mensagem: ${mensagem}
             user: 'contato@agenciagalharufa.com.br',
             pass: 'agencia1234'
         }
-    })
+    });
 
     transporter.sendMail(mailOptions).then(err => {
         if (err.rejected.length > 0) return res.status(400).json(err);
         return res.status(200).json({ message: 'ok' });
     })
-})
+});
 
-module.exports = router
+module.exports = router;
