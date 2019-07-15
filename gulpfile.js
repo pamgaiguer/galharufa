70430d0636054e48d85eed1c62beb2a7d9dabe47
Pam Gaiguer <pamella.gaiguer@gmail.com>
2019-07-15 23:24:35 +1000

    commiting css

--- gulpfile.js
+++ gulpfile.js
@@ -5,4 +5,15 @@ gulp.task('sass', function(){
     return gulp.src('./www/styles/scss/custom.scss')
       .pipe(sass()) // Using gulp-sass
       .pipe(gulp.dest('./www/styles/css'))
+});
+
+gulp.task('hello', function() {
+    console.log('Hello Zell');
+  });
+
+  gulp.task('default', function (callback) {
+    // runSequence(['sass','browserSync', 'watch'],
+    runSequence(['sass','browserSync', 'watch'],
+      callback
+    )
   });
