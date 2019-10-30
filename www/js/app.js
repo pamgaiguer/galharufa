5c29d079f8626da99f62051d3710a9e733252f5c
Pam Gaiguer <pamella.gaiguer@gmail.com>
2019-10-30 11:35:31 +1000

    Adding parameter to close the sidenav bar

--- www/js/app.js
+++ www/js/app.js
@@ -21,108 +21,111 @@ var app = angular.module('galharufa', [
   'galharufa.services.UserServices',
   'galharufa.services.CastingServices',
   'galharufa.services.SimpleServices']);
-
-app.run(function($rootScope, $timeout, UserServices, $location, $route) {
-
-  $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
-    $dialogs.error("Something went wrong!", error);
-    console.error("$stateChangeError: ", toState, error);
-  });
-
-  $rootScope.headerId = 1;
-  $rootScope.hold = false;
-  UserServices.loadUserLocalStorage();
-
-  $rootScope.path = function(p,params={}){
-    if(params && Object.keys(params).length){
-      $location.path(p).search(params);
-    }else{
-      $location.path(p);
+  
+  app.run(function($rootScope, $timeout, UserServices, $location, $route) {
+    
+    $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
+      $dialogs.error("Something went wrong!", error);
+      console.error("$stateChangeError: ", toState, error);
+    });
+    
+    $rootScope.headerId = 1;
+    $rootScope.hold = false;
+    UserServices.loadUserLocalStorage();
+    
+    $rootScope.path = function(p,params={}){
+      if(params && Object.keys(params).length){
+        $location.path(p).search(params);
+      }else{
+        $location.path(p);
+      }
+      $route.reload();
     }
-    $route.reload();
-  }
-
-  $rootScope.logout = function(){
-    UserServices.deleteUserLocalStorage();
-    $location.path('/login');
-  }
-})
-
-app.constant('Configs', {
-  appToken:'',
-  // apiUrl:'http://localhost:3000/'
-  apiUrl:'http://agenciagalharufa.com.br/'
-  // apiUrl:'https://agenciagalharufa.com.br/'
-});
-
-app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-
-  $routeProvider
-  .when('/', {
-    templateUrl: '/templates/home.html',
-    controller: 'HomeCtrl'
+    
+    $rootScope.logout = function(){
+      UserServices.deleteUserLocalStorage();
+      $location.path('/login');
+    }
   })
-  .when('/home', {
-    templateUrl: '/templates/home.html',
-    controller: 'HomeCtrl'
-  })
-  .when('/login/', {
-    templateUrl: '/templates/login.html',
-    controller: 'LoginCtrl'
-  })
-  .when('/contato/', {
-    templateUrl: '/templates/contato.html',
-    controller: 'ContatoCtrl'
-  })
-  .when('/servicos/', {
-    templateUrl: '/templates/servicos.html',
-    controller: 'ServicosCtrl'
-  })
-  .when('/casting/', {
-    templateUrl: '/templates/casting.html',
-    controller: 'CastingCtrl'
-  })
-  .when('/casting-details', {
-    templateUrl: '/templates/casting-details.html',
-    controller: 'CastingDetailsCtrl'
-  })
-  .when('/casting-details/:id', {
-    templateUrl: '/templates/casting-details.html',
-    controller: 'CastingDetailsCtrl'
-  })
-  .when('/quemsomos/', {
-    templateUrl: '/templates/quemsomos.html',
-    controller: 'QuemsomosCtrl'
-  })
-  .when('/adm-home/', {
-    templateUrl: '/templates/adm-home.html',
-    controller: 'AdminHomeCtrl'
-  })
-  .when('/adm-casting-register/', {
-    templateUrl: '/templates/adm-casting-register.html',
-    controller: 'AdminCastingRegCtrl'
-  })
-  .when('/adm-casting-search/', {
-    templateUrl: '/templates/adm-casting-search.html',
-    controller: 'AdminCastingSearchCtrl'
-  })
-  .when('/adm-casting-show/:id', {
-    templateUrl: '/templates/adm-casting-show.html',
-    controller: 'AdminCastingShowCtrl'
-  })
-  .when('/adm-users/', {
-    templateUrl: '/templates/adm-users.html',
-    controller: 'AdminUsersCtrl'
-  })
-  .when('/adm-quemsomos/', {
-    templateUrl: '/templates/adm-quemsomos.html',
-    controller: 'AdminQuemsomosCtrl'
-  })
-  .when('/adm-servicos/', {
-    templateUrl: '/templates/adm-servicos.html',
-    controller: 'AdminServicosCtrl'
+  
+  app.constant('Configs', {
+    appToken:'',
+    // apiUrl:'http://localhost:3000/'
+    apiUrl:'http://agenciagalharufa.com.br/'
+    // apiUrl:'https://agenciagalharufa.com.br/'
   });
-
-    $locationProvider.html5Mode(true);
-
-}]);
+  
+  app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider
+    .when('/', {
+      templateUrl: '/templates/home.html',
+      controller: 'HomeCtrl'
+    })
+    .when('/home', {
+      templateUrl: '/templates/home.html',
+      controller: 'HomeCtrl'
+    })
+    .when('/login/', {
+      templateUrl: '/templates/login.html',
+      controller: 'LoginCtrl'
+    })
+    .when('/contato/', {
+      templateUrl: '/templates/contato.html',
+      controller: 'ContatoCtrl'
+    })
+    .when('/servicos/', {
+      templateUrl: '/templates/servicos.html',
+      controller: 'ServicosCtrl'
+    })
+    .when('/casting/', {
+      templateUrl: '/templates/casting.html',
+      controller: 'CastingCtrl'
+    })
+    .when('/casting-details', {
+      templateUrl: '/templates/casting-details.html',
+      controller: 'CastingDetailsCtrl'
+    })
+    .when('/casting-details/:id', {
+      templateUrl: '/templates/casting-details.html',
+      controller: 'CastingDetailsCtrl'
+    })
+    .when('/quemsomos/', {
+      templateUrl: '/templates/quemsomos.html',
+      controller: 'QuemsomosCtrl'
+    })
+    .when('/adm-home/', {
+      templateUrl: '/templates/adm-home.html',
+      controller: 'AdminHomeCtrl'
+    })
+    .when('/adm-casting-register/', {
+      templateUrl: '/templates/adm-casting-register.html',
+      controller: 'AdminCastingRegCtrl'
+    })
+    .when('/adm-casting-search/', {
+      templateUrl: '/templates/adm-casting-search.html',
+      controller: 'AdminCastingSearchCtrl'
+    })
+    .when('/adm-casting-show/:id', {
+      templateUrl: '/templates/adm-casting-show.html',
+      controller: 'AdminCastingShowCtrl'
+    })
+    .when('/adm-users/', {
+      templateUrl: '/templates/adm-users.html',
+      controller: 'AdminUsersCtrl'
+    })
+    .when('/adm-quemsomos/', {
+      templateUrl: '/templates/adm-quemsomos.html',
+      controller: 'AdminQuemsomosCtrl'
+    })
+    .when('/adm-servicos/', {
+      templateUrl: '/templates/adm-servicos.html',
+      controller: 'AdminServicosCtrl'
+    });
+    $locationProvider.html5Mode({
+      enabled:true,
+      requireBase:false
+    })
+    .hashPrefix(''); ;
+    
+  }]);
+  
