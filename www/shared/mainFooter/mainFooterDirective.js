9d66907af12bded358d43ebb9790bcbfa823ed38
Pam Gaiguer <pamella.gaiguer@gmail.com>
2019-11-19 13:15:08 +1000

    new formal layout

--- www/shared/mainFooter/mainFooterDirective.js
+++ www/shared/mainFooter/mainFooterDirective.js
@@ -1,27 +1,29 @@
 (function() {
-        'use strict';
-
-        var mainFooter = function() {
-            return {
-                restricted: 'E',
-                templateUrl: '/shared/mainFooter/mainFooterTemplate.html',
-                controller: 'mainFooterController as footerCtrl'
-            };
+    'use strict';
+    
+    var mainFooter = function() {
+        return {
+            restricted: 'E',
+            templateUrl: '/shared/mainFooter/mainFooterTemplate.html',
+            controller: 'mainFooterController as footerCtrl'
         };
-
-        var mainFooterController = function() {
-            var self = this;
-            var init = function() {
-
-            }
-            init();
-
-            self.channelWarn = function() {
-                Materialize.toast('Canal no youtube ainda em desenvolvimento!', 4000);
-            }
-
-        };
-
+    };
+    
+    var mainFooterController = function() {
+        var self = this;
+        this.date = new Date();
+        var init = function() {
+            
+        }
+        init();
+        
+        self.channelWarn = function() {
+            Materialize.toast('Canal no youtube ainda em desenvolvimento!', 4000);
+        }
+        
+    };
+    
+    
     angular.module('galharufa')
     .controller('mainFooterController', [
         mainFooterController
